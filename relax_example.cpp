69323dbeeef71d7f1127fbfa363770ea6ecb5558
KatzeCode <sebasdriven15@gmail.com>
2023-01-08 12:23:40 +0000

    Precision to stop added to the code

--- relax_example.cpp
+++ relax_example.cpp
@@ -12,27 +12,29 @@ const double ymin = 0.0;
 const double ymax = 1.2;
 const int nx = (xmax - xmin) / delta;
 const int ny = (ymax - ymin) / delta;
-const int nsteps = 200;
+const int maxnsteps = 1000;
 
 // Function declarations
 void initial_conditions(data_t &data, int nx, int ny);
 void boundary_conditions(data_t &data, int nx, int ny);
-void relaxation_step(data_t &data, int nx, int ny);
+double relaxation_step(data_t &data, int nx, int ny);
 void print_screen(const data_t &data, int nx, int ny);
 void start_gnuplot(void);
 void print_gnuplot(const data_t &data, int nx, int ny);
-void evolve(data_t &data, int nx, int ny, int nsteps);
+int evolve(data_t &data, int nx, int ny, int maxnsteps, double eps);
 //
 
 // Main
 int main() {
 
   data_t potential(nx * ny);
+  double eps = 1.0e-3;
 
   initial_conditions(potential, nx, ny);
   boundary_conditions(potential, nx, ny);
 
-  evolve(potential, nx, ny, nsteps);
+  int actual_nsteps = evolve(potential, nx, ny, maxnsteps, eps);
+  std::cout << "quit\n";
 
   return 0;
 }
@@ -76,21 +78,24 @@ void boundary_conditions(data_t &data, int nx, int ny) {
   }
 }
 
-void relaxation_step(data_t &data, int nx, int ny) {
-  // Recorre la matriz cuidando de no modificar las condiciones de frontera.
+double relaxation_step(data_t &data, int nx, int ny) {
+  double maxDELTA = -1;
   for (int ix = 1; ix < nx - 1; ++ix) {
     for (int iy = 1; iy < ny - 1; ++iy) {
-      // Revisar que la celda no es condición de frontera. if (si es condición)
-      // {continue;}
       if ((ix == nx / 2) && ((ny / 3) <= iy) && (iy <= 2 * (ny / 3))) {
         continue;
       }
-      data[ix * ny + iy] =
-          (data[(ix + 1) * ny + iy] + data[(ix - 1) * ny + iy] +
-           data[ix * ny + (iy + 1)] + data[ix * ny + (iy - 1)]) /
-          4.0;
+      double newval = (data[(ix + 1) * ny + iy] + data[(ix - 1) * ny + iy] +
+                       data[ix * ny + (iy + 1)] + data[ix * ny + (iy - 1)]) /
+                      4.0;
+      double newDELTA = std::fabs(1 - (newval / data[ix * ny + iy]));
+      if (newDELTA > maxDELTA) {
+        maxDELTA = newDELTA;
+      }
+      data[ix * ny + iy] = newval;
     }
   }
+  return maxDELTA;
 }
 
 void print_screen(const data_t &data, int nx, int ny) {
@@ -123,11 +128,16 @@ void print_gnuplot(const data_t &data, int nx, int ny) {
   std::cout << "e\n";
 }
 
-void evolve(data_t &data, int nx, int ny, int nsteps) {
+int evolve(data_t &data, int nx, int ny, int maxnsteps, double eps) {
   start_gnuplot();
-  for (int istep = 0; istep < nsteps; ++istep) {
-    relaxation_step(data, nx, ny);
+  int istep;
+  for (istep = 0; istep < maxnsteps; ++istep) {
+    double maxDELTA = relaxation_step(data, nx, ny);
+    if (maxDELTA <= eps) {
+      break;
+    }
     print_gnuplot(data, nx, ny);
   }
+  return istep;
 }
 //
